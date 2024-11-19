207569bd48c8873b409f1560d8b8f77536d0e1a0
Rana Dawar Abdullah <imdawar090@gmail.com>
2024-11-20 00:46:58 +0500

    few updates in the product listings

--- components/global/ProductListing.tsx
+++ components/global/ProductListing.tsx
@@ -1,5 +1,6 @@
 import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { COLORS } from "@/constants/theme";
 
 interface ProductListingProps {
   discount?: number;
@@ -11,6 +12,7 @@ interface ProductListingProps {
   afterDiscount?: number;
   onPress: () => void;
   onPressAddToCart: () => void;
+  isFav?: boolean;
 }
 
 const ProductListing: React.FC<ProductListingProps> = ({
@@ -21,12 +23,31 @@ const ProductListing: React.FC<ProductListingProps> = ({
   rating,
   totalFeedbacks,
   afterDiscount,
+
   onPress,
 }) => {
   return (
     <Pressable style={styles.mainContainer} onPress={onPress}>
+      <View style={styles.imageContainer}>
+        <Image resizeMode="contain" source={image} style={styles.image} />
+        {discount > 0 && (
+          <View style={styles.discountShow}>
+            <Text
+              adjustsFontSizeToFit
+              numberOfLines={1}
+              style={styles.discountText}
+            >
+              {discount}%
+            </Text>
+          </View>
+        )}
+
+        <View>
+          <MaterialCommunityIcons name={"heart-outlined"} />
+        </View>
+      </View>
       <View>
-        <Image source={image} />
+        <Text>{title}</Text>
       </View>
     </Pressable>
   );
@@ -34,4 +55,19 @@ const ProductListing: React.FC<ProductListingProps> = ({
 
 export default ProductListing;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: 100,
+    height: 100,
+  },
+  imageContainer: {
+    height: 150,
+    width: 125,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: COLORS.grayBg,
+  },
+  discountShow: {
+    backgroundColor: COLORS.third,
+  },
+});
